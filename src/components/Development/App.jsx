aec34b339425917843ea1959d038bbe131fca4b6
Kartik-Ahuja <thekartikahuja2005@gmail.com>
2023-09-15 09:42:53 +0530

    adding projects in app development

--- src/components/Development/App.jsx
+++ src/components/Development/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-// import YourBajaar from '../Development/App Dev/yourbajar.png'
+import YourBajaar from '../Development/App Dev/YourBajar.png'
+import CoronaTest from '../Development/App Dev/TestingApp.png'
 import '../Development/style.css'
 import Info from '../Contactbar/info'
 
@@ -8,13 +9,19 @@ const AppDev = () => {
 
         {
             projectNo: "01.",
-            ProjectName: <a href="https://yourbajaar.com/ " target='_blank'> Your Bajar </a>,
+            ProjectName: <a href="https://yourbajaar.com/ " target='_blank'> Your Bajar (E-Commerce) </a>,
             Description: "Step into a shopping revolution with 'YOUR BAJAR,' where we've completely transformed your everyday essentials shopping experience. Whether it's kitchen staples or your daily must-haves, 'YOUR BAJAR' is your ultimate one-stop destination. Get ready for secure transactions, handpicked product recommendations, and lightning-fast deliveries, all meticulously crafted to streamline your daily routine. 'YOUR BAJAR' isn't just an app; it's your key to effortless shopping for a smoother daily life",
-            // Image: YourBajaar,
+            Image: YourBajaar,
             ImageLink: "https://yourbajaar.com/"
-
-
         },
+        {
+            projectNo: "02.",
+            ProjectName: <a href="https://testcenter-heidelberg.de/ " target='_blank'> TESTCENTER Heidelberg (Healthcare Application)</a>,
+            Description: "TESTCENTER Heidelberg was a prominent COVID-19 testing center and service located in Heidelberg, Germany. This testing facility provided essential COVID-19 testing services, including PCR and rapid tests, to assist individuals in determining their infection status during the ongoing pandemic. Please note that the details mentioned here are based on information available at that time, and I recommend visiting their official website or contacting them directly for the most up-to-date information regarding their testing capabilities and procedures.",
+            Image: CoronaTest,
+            ImageLink: "https://testcenter-heidelberg.de/"
+        },
+
 
     ]
     return (
--- src/components/Development/MLM.jsx
+++ src/components/Development/MLM.jsx
@@ -7,20 +7,13 @@ const MLM = () => {
     const PortfolioProjects = [
         {
             projectNo: "01.",
-            ProjectName: "First project",
-            Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ad iure maxime asperiores! Quod alias eius, ad debitis ab consequuntur vero. Culpa quas sunt maxime laborum, itaque distinctio deleniti accusamus!elit. Quidem ad iure maxime asperiores! Quod alias eius, ad debitis ab consequuntur vero. Culpa quas sunt maxime laborum, itaque distinctio deleniti accusamus!elit. Quidem ad iure maxime asperiores! Quod alias eius, ad debitis ab consequuntur vero. Culpa quas sunt maxime laborum, itaque distinctio deleniti accusamus!",
-            Image: firstimage
+            ProjectName: <a href="https://www.reddottours.com/" target='_blank'>HAWWKS </a>,
+            Description: "The Red Dot Tours website is a comprehensive online platform specializing in travel services and experiences, offering a diverse range of destination information, meticulously crafted travel packages, and convenient booking options for accommodations, flights, and more. With stunning visuals, customer reviews, and informative travel guides, it caters to travelers seeking to explore and discover their dream destinations, while an 'About Us' section provides insights into the company's ethos and expertise. Whether you're planning your next adventure or simply seeking inspiration, Red Dot Tours aims to be your go-to resource for all things travel.",
+            // Image: HAWWKS,
+            ImageLink: "https://www.reddottours.com/"
 
 
         },
-        {
-            projectNo: "01.",
-            ProjectName: "First project",
-            Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ad iure maxime asperiores! Quod alias eius, ad debitis ab consequuntur vero. Culpa quas sunt maxime laborum, itaque distinctio deleniti accusamus!elit. Quidem ad iure maxime asperiores! Quod alias eius, ad debitis ab consequuntur vero. Culpa quas sunt maxime laborum, itaque distinctio deleniti accusamus!elit. Quidem ad iure maxime asperiores! Quod alias eius, ad debitis ab consequuntur vero. Culpa quas sunt maxime laborum, itaque distinctio deleniti accusamus!",
-            Image: firstimage
-
-
-        }
     ]
     return (
         <>
@@ -33,10 +26,9 @@ const MLM = () => {
                 <div key={index} className="container">
                     <h1 className="projectName">{content.projectNo} {content.ProjectName} </h1>
                     <div className="description">{content.Description}</div>
-
-                    <img src={content.Image} alt="" className="projectImage" />
-
-
+                    <a href={content.ImageLink} target='_blank'>
+                        <img src={content.Image} alt="" className="projectImage" />
+                    </a>
                 </div >
             ))}
             <Info />
